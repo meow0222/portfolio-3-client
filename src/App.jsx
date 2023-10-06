8a1608236ae1982f34eb87a224664f56dccf1203
meow0222 <meow222.csgo@gmail.com>
2023-10-06 11:25:19 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import Nav from './components/Nav.jsx'
+import Hero from './components/Hero.jsx'
+import Footer from './components/Footer.jsx'
+import Contact from './components/Contact.jsx'
+
+// function fetchData 
+
+function App() {
+  return (
+    <>
+      <Nav />
+      <Hero />
+      <Contact />
+      <Footer />
+    </>
+  )
+}
+
+export default App
